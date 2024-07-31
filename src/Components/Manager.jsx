00f55340960a341cfc42c3089547e2dd286997d3
Mainul Islam <maainul.islam@gmail.com>
2024-07-31 16:38:23 +0600

    Implement Manager component design

--- src/Components/Manager.jsx
+++ src/Components/Manager.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
 
 export default function Manager() {
   return (
-    <div>
-      manage your  password!!!!!11
-    </div>
-  )
+    <>
+      <div className="absolute inset-0 -z-10 h-full w-full bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
+        <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-green-400 opacity-20 blur-[100px]"></div>
+      </div>
+
+      <div className=" p-3 md:mycontainer min-h-[88.2vh] ">
+        <h1 className="text-4xl text font-bold text-center">
+          <span className="text-green-500"> &lt;</span>
+          <span>Pass</span>
+          <span className="text-green-500">OP/&gt;</span>
+        </h1>
+        <p className='text-green-900 text-lg text-center'>Your own Password Manager</p>
+
+        <div className="flex flex-col p-4 text-black gap-8 items-center">
+            <input placeholder='Enter website URL' className='rounded-full border border-green-500 w-full p-4 py-1' type="text" name="site" id="site" />
+            <div className="flex flex-col md:flex-row w-full justify-between gap-8">
+                <input   placeholder='Enter Username' className='rounded-full border border-green-500 w-full p-4 py-1' type="text" name="username" id="username" />
+                <div className="relative">
+                    <input    placeholder='Enter Password' className='rounded-full border border-green-500 w-full p-4 py-1' type="password" name="password" id="password" />
+                    <span className='absolute right-[3px] top-[4px] cursor-pointer' >
+                        <img  className='p-1' width={26} src="icons/eye.png" alt="eye" />
+                    </span>
+                </div>
+
+            </div>
+            <button  className='flex justify-center items-center gap-2 bg-green-400 hover:bg-green-300 rounded-full px-8 py-2 w-fit border border-green-900'>
+                <lord-icon
+                    src="https://cdn.lordicon.com/jgnvfzqg.json"
+                    trigger="hover" >
+                </lord-icon>
+                Save</button>
+        </div>
+      </div>
+    </>
+  );
 }
