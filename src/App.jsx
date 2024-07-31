7dec2f4483bc38296fd03ddc1f3329af5ba4b6d2
Mainul Islam <maainul.islam@gmail.com>
2024-07-31 14:00:54 +0600

    Implement footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './Components/Navbar'
+import Manager from './Components/Manager'
+import Footer from './Components/Footer'
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <Manager />
+      <Footer />
     </>
   )
 }
---
+++ src/Components/Manager.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Manager() {
+  return (
+    <div>
+      manage your  password!!!!!11
+    </div>
+  )
+}
