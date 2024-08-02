d71cfaffddbdf0abaca090bba11a635afa52d085
Mainul Islam <maainul.islam@gmail.com>
2024-08-02 18:51:18 +0600

    change deploy script

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { ToastContainer } from 'react-toastify'
+import './App.css'
+import Navbar from './Components/Navbar'
+import Manager from './Components/Manager'
+import Footer from './Components/Footer'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
+function App() {
+
+  return (
+    <BrowserRouter basename="password-manager">
+        <Routes>
+        <Route 
+          path="/"
+          element={
+            <>
+          <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+            transition="Bounce"
+            />
+          <ToastContainer />
+          <Navbar />
+          <div className=' bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]'>
+          <Manager />
+          </div>
+          <Footer /> 
+          </>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
