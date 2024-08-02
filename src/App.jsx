80cbcd80e720e31b792660f6ab419fdac2ddc3bf
Mainul Islam <maainul.islam@gmail.com>
2024-08-02 18:16:50 +0600

    Add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,40 @@ import './App.css'
 import Navbar from './Components/Navbar'
 import Manager from './Components/Manager'
 import Footer from './Components/Footer'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 function App() {
 
   return (
-    <>
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-        transition="Bounce"
-        />
-      <ToastContainer />
-      <Navbar />
-      <div className=' bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]'>
-      <Manager />
-      </div>
-      <Footer />
-    </>
+    <BrowserRouter basename="todo-react">
+        <Routes>
+        <Route 
+          path="/"
+          element={
+            <>
+          <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+            transition="Bounce"
+            />
+          <ToastContainer />
+          <Navbar />
+          <div className=' bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]'>
+          <Manager />
+          </div>
+          <Footer /> 
+          </>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
